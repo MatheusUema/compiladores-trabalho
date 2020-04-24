5a7f056891a405361e561b1460b6eeb7d1cd8681
Matheus <matheuema@gmail.com>
2020-04-23 22:27:12 -0300

    programa

--- main.c
+++ main.c
@@ -12,22 +12,47 @@ int main(int argc, char *argv[]) { //Compilador
         return 0;
     }
     
-    FILE *arquivo_entrada  = fopen(argv[1], "r");
-    if (arquivo_entrada == NULL) {   
-        printf("[ERRO] Arquivo não encontrado\n"); 
-        return 0;
-    } 
 
     /* Função que chama o Analisador Léxico */
-    analisador_lexico(arquivo_entrada);
+    analisador_lexico(argv[1]);
 
 
 
 }
 
-int analisador_lexico (FILE *arquivo){
+int analisador_lexico (char *arquivo){
     
+    FILE *arquivo_entrada = fopen(arquivo, "r");
+    FILE *arquivo_saida = fopen("saida.txt", "w"); 
+    if (arquivo_entrada == NULL) {   
+        printf("[ERRO] Arquivo não encontrado\n"); 
+        return 0;
+    } 
+
+    char caract;
+    caract = fgetc(arquivo_entrada);
+    while(fgetc(arquivo_entrada) != 32) {
+        printf("aaaa\n");
+    }
 
 
 
+
+    fclose(arquivo_entrada);
+    fclose(arquivo_saida);
+}
+
+int confere_numero (char caract){
+    //Confere se o caractere é numero, checando seu valor na tabela ASCII
+    if (caract >= 48 && caract <= 57){
+        return 1;
+    } 
+    return 0;
+}
+
+int confere_letra (char caract) {
+     if ((caract >= 65 && caract <= 90) || (caract >= 97 && caract <= 122)){
+        return 1;
+    } 
+    return 0;
 }
--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int analisador_lexico(FILE *arquivo);
+int analisador_lexico(char *arquivo);
 
+int confere_numero (char caract);
 #endif
